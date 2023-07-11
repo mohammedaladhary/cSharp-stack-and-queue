f64efa2ac5ae5e4d8465dfd335bc1bc40f0aa1e1
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-11 13:59:26 +0400

    queue implementation using an array

--- Program.cs
+++ Program.cs
@@ -5,17 +5,42 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //---------------------stack-----------------------
+            //Stack stack = new Stack(5);
 
-            Stack stack = new Stack(5);
+            //stack.push(10);
+            //stack.push(20);
+            //stack.push(30);
+            //stack.push(40);
+            //stack.push(45);
+            //stack.pop();
+            //stack.pop();
+            //stack.peek();
 
-            stack.push(10);
-            stack.push(20);
-            stack.push(30);
-            stack.push(40);
-            stack.push(45);
-            stack.pop();
-            stack.pop();
-            stack.peek();
+            //---------------------queue----------------------
+            Queue queue = new Queue(4);
+
+            // inserting elements in the queue
+            queue.enqueue(20);
+            queue.enqueue(30);
+            queue.enqueue(40);
+            queue.enqueue(50);
+
+            // print Queue elements
+            queue.queueDisplay();
+
+            // queue is full
+            queue.enqueue(60); 
+
+            queue.dequeue();
+            queue.dequeue();
+
+            //display updated queue after deleting some elements
+            Console.WriteLine("updated queue: ");
+            queue.queueDisplay();
+
+            // print front of the queue
+            queue.peek();
         }
     }
 }
--- Queue.cs
+++ Queue.cs
@@ -8,5 +8,94 @@ namespace stack_and_queue
 {
     internal class Queue
     {
+        private int front;
+        private int rear;
+        private int size;
+        private int[] queue;
+
+        public Queue(int c)
+        {
+            front = 0;
+            rear = 0;
+            size = c;
+            queue = new int[size];
+        }
+        private bool ifFull()
+        {
+            return size == rear;
+        }
+        private bool isEmpty()
+        {
+            return front == rear;
+        }
+        public void enqueue(int value)
+        {
+            // check if queue is full
+            if (ifFull())
+            {
+                Console.WriteLine($"Queue is full to add {value}");
+                return;
+            }
+
+            else
+            {
+                queue[rear++] = value;
+            }
+            return;
+        }
+
+        public void dequeue()
+        {
+            // if queue is empty
+            if (isEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            // 5 <-- 6 <-- 7  5 is the rear here and will be removed 
+            else
+            {
+                for (int i = 0; i < rear - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+
+                if (rear < size)
+                    queue[rear] = 0;
+
+                rear--;
+            }
+            return;
+        }
+
+        // print queue elements
+        public void queueDisplay()
+        {
+            int i;
+            if (isEmpty())
+            {
+                Console.Write("Queue is Empty");
+                return;
+            }
+
+            // traverse front to rear and print elements
+            for (i = front; i < rear; i++)
+            {
+                Console.WriteLine(queue[i]);
+            }
+            return;
+        }
+
+        // print front of queue
+        public void peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            Console.Write("Peek(front) Element is:" +queue[front]);
+            return;
+        }
     }
 }
