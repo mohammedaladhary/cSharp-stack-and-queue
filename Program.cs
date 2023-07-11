4bceb756b2a4f1c48e4313075815ce4892a77a5e
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-11 13:22:12 +0400

    stack implementation

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace stack_and_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Stack stack = new Stack(5);
+
+            stack.push(10);
+            stack.push(20);
+            stack.push(30);
+            stack.push(40);
+            stack.push(45);
+            stack.pop();
+            stack.pop();
+            stack.peek();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_and_queue
+{
+    internal class Stack
+    {
+        private int[] elements;
+        private int top;
+
+        public Stack(int size)
+        {
+            this.elements = new int[size];
+            top = -1;
+        }
+
+        public void push(int value)
+        {
+            //if (top >= elements.Length - 1) // if the stuck is full
+            if (isFull())
+            {
+                Console.WriteLine("The Stack is Full");
+                return;
+            }
+            elements[++top] = value;
+        }
+        public int pop()
+        {
+            //if (top == -1) // if the stack is empty
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            return elements[top--];
+        }
+        public int peek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            return elements[top];
+        }
+        private bool isEmpty()
+        {
+            return top == -1;
+        }
+        private bool isFull()
+        {
+            return top >= elements.Length - 1;
+        }
+    }
+}
